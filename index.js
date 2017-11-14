91b586c0066798708239258b20c313cde0b53648
lexkrehbiel <lexkrehbiel@gmail.com>
2017-11-14 16:54:23 -0500

    __

--- index.js
+++ index.js
@@ -25,6 +25,32 @@ app.engine('html', require('ejs').renderFile);
 // anything at /static, reference the public directory
 app.use('/static',express.static('public'))
 
+app.get('/sign-s3', (req, res) => {
+  const s3 = new aws.S3();
+  const fileName = req.query['file-name'];
+  const fileType = req.query['file-type'];
+  const s3Params = {
+    Bucket: S3_BUCKET,
+    Key: fileName,
+    Expires: 60,
+    ContentType: fileType,
+    ACL: 'public-read'
+  };
+
+  s3.getSignedUrl('putObject', s3Params, (err, data) => {
+    if(err){
+      console.log(err);
+      return res.end();
+    }
+    const returnData = {
+      signedRequest: data,
+      url: `https://${S3_BUCKET}.s3.amazonaws.com/${fileName}`
+    };
+    res.write(JSON.stringify(returnData));
+    res.end();
+  });
+});
+
 // for /upload, run the uploadSequence function
 router.use('/upload', uploadSequence);
 
